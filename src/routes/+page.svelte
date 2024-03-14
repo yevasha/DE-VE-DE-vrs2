27b2369355bd9382b19469b25f8d44c1d1209840
Gabriel Jimenez <gabrieljimenez79@hotmail.com>
2024-03-14 11:01:45 +0100

    trying to merge to pull

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,85 +1,77 @@
 <script>
   import { async } from "@firebase/util";
   import { firestore } from "$lib/firebase-config";
-  import { collection, addDoc ,getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
-  
-  console.log('firestore',firestore)
+  import {
+    collection,
+    addDoc,
+    getDocs,
+    updateDoc,
+    doc,
+    deleteDoc,
+  } from "firebase/firestore";
+
+  console.log("firestore", firestore);
 
   async function getMovie() {
-  try {
-    const movieCollection = await getDocs(collection(firestore, "movies"));
+    try {
+      const movieCollection = await getDocs(collection(firestore, "movies"));
       movieCollection.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   }
 
-
-async function addMovie() {
-  try {
-    const moviesCollectionRef = await addDoc(collection(firestore, "movies"), {
-      titel: "Eva",
-      genre: "Komedi",
-      releaseDate: "25/12/2003",
-      watched: false
-    });
-      console.log('try to yeet Eva ',moviesCollectionRef)
+  async function addMovie() {
+    try {
+      const moviesCollectionRef = await addDoc(
+        collection(firestore, "movies"),
+        {
+          titel: "Eva",
+          genre: "Komedi",
+          releaseDate: "25/12/2003",
+          watched: false,
+        },
+      );
+      console.log("try to yeet Eva ", moviesCollectionRef);
       console.log("Document written with ID: ", moviesCollectionRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
+  }
+
+  async function updateMovie() {
+    try {
+      // Add a new document in collection "cities"
+      await setDoc(doc(db, "movies", "movie"), {
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA",
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
+  }
 
+  // Delete me here
+  async function deleteMovie() {
+    try {
+      await deleteDoc(doc(firestore, "movies", "movie"));
 
-async function updateMovie() {
-  try {
-// Add a new document in collection "cities"
-await setDoc(doc(db, "cities", "movie"), {
-  name: "Los Angeles",
-  state: "CA",
-  country: "USA"
-});
-
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-}
-
-
-
-
-
-// Delete me here 
-async function deleteMovie() {
-  try {
-
-    await deleteDoc(doc(firestore, "movies", "movie"));   
-    
-  console.log("Byeeee tihi ") 
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-}
-
-
-
-
-
-
+      console.log("Byeeee tihi ");
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
 </script>
-
-
-
-
-
 
 <h1 class="p-4 m-2 text-3xl font-bold">DE-VE-DE Movie Database</h1>
 
-
 <button class="btn" on:click={addMovie}> yeeet</button>
-<button class="btn btn-secondary" on:click={getMovie}> Give me the chockladboll</button>
+<button class="btn btn-secondary" on:click={getMovie}>
+  Give me the chockladboll</button
+>
 <button class="btn btn-neutral" on:click={deleteMovie}> meow</button>
 
 <div class="hero min-h-screen bg-base-200">
